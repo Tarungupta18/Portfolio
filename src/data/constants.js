aa1db8904aa9842ee525679299ce23b61eb17814
tarun gupta <tkg21430@gmail.com>
2024-04-11 00:27:10 +0530

    add

---
+++ src/data/constants.js
@@ -0,0 +1,294 @@
+export const Bio = {
+  name: "Tarun Gupta",
+  roles: [
+    "Full Stack Developer",
+    "MERN Stack Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Tarungupta18",
+  resume:
+    "https://drive.google.com/file/d/1kVQhhlKJovupBnBrkDQLZLRmSLVBj8ho/view?usp=drivesdk",
+  linkedin: "https://www.linkdin.com/in/tarun-gupta-100a1b259",
+  twitter: "",
+  insta: "https://www.instagram.com/tarungupta_11/",
+  facebook: "",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      // {
+      //   name: "Angular Js",
+      //   image:
+      //     "https://camo.githubusercontent.com/8886130b3d8aba95dbdd7c4f9a41029606424cc06d1873c1ced87dd55a222fef/68747470733a2f2f616e67756c61722e696f2f6173736574732f696d616765732f6c6f676f732f616e67756c61722f616e67756c61722e737667",
+      // },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind",
+        image:
+          "https://www.svgrepo.com/show/374118/tailwind.svg",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsL7a_rhUx0yrF7FRmTTHbjJOCHtbWRGSdXCaPatbl7A&s",
+      },
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://ajeetchaulagain.com/static/7cb4af597964b0911fe71cb2f8148d64/87351/express-js.png",
+      },
+      {
+        name: "Rest APIs",
+        image: "https://www.iconpacks.net/icons/free-icons-6/free-rest-api-blue-logo-icon-22098-thumb.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "EJS",
+        image:
+          "https://www.svgrepo.com/show/373574/ejs.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "API",
+        image: "https://cdn-icons-png.flaticon.com/512/627/627558.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "C",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg",
+      },
+      {
+        name: "C++",
+        image:
+          "https://play-lh.googleusercontent.com/RAA-PGAoUanS_aOu8qhDvQZ1d8jp_lhR_-zJrcvxpABgQtNQylCxNac_WvN-vVIz7Hw=w240-h480-rw",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "SQL",
+        image: "https://www.svgrepo.com/show/331760/sql-database-generic.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Render",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://collegekhabri.com/data/shri-ram-institute-of-technology-logo.jpg",
+    school: "Shri Ram Institute of Science & Technology, Jabalpur",
+    date: "Oct 2021 - Sep 2025",
+    grade: "8.44 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Shri Ram Institute of Science & Technology, Jabalpur. I have completed 5 semesters and have a CGPA of 8.44. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSCs0ySdpFl7hF37s4-2m5NJSrKAfvEWQAcKlCj0saLNFhaxS1TCBNJ85spZFsPjjJtdM&usqp=CAU",
+    school: "Vidya Sagar Senior Sec. School, Burhar",
+    date: "Apr 2020 - Apr 2021",
+    grade: "85.6%",
+    desc: "I completed my class 12 high school education at Vidya Sagar Senior Sec. School, Burhar, where I studied PCM - Physics, Chemistry, Math.",
+    degree: "MPBOARD(XII), PCM",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSCs0ySdpFl7hF37s4-2m5NJSrKAfvEWQAcKlCj0saLNFhaxS1TCBNJ85spZFsPjjJtdM&usqp=CAU",
+    school: "Vidya Sagar Senior Sec. School, Burhar",
+    date: "Apr 2018 - Apr 2019",
+    grade: "86.2%",
+    desc: "I completed my class 10 education at Vidya Sagar Senior Sec. School, Burhar, where I studied Science, Math and  other subjects.",
+    degree: "MPBOARD(X), Science & Computer",
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://github.com/Tarungupta18/upload-img/blob/aim/portfolio-img/cisco-icon.png?raw=true",
+    role: "Virtual Cyber Security Intern",
+    company: "Cisco Pvt. Ltd.",
+    date: "Aug 2023 - June 2023",
+    desc: "Working on Packet Tracer Platforms, creating College Network module/branch.",
+    skills: [
+      "Packet Flow",
+      "Routers",
+      "Packet Tracer",
+    ],
+    doc: "https://drive.google.com/file/d/14UmFFXe0veetA7Fvx7XkZ47wTyO_CdqA/view?usp=drivesdk",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/D560BAQEZKRKOWBcJ_A/company-logo_200_200/0/1706735138636/octanetsolutions_logo?e=2147483647&v=beta&t=x4pBgtyLAr0gBAUCImcbOCCmhOTWB8B8dkXMSA5lde0",
+    role: "Web Developer Intern",
+    company: "Octanet pvt. ltd.",
+    date: "Feb 2024 - March 2024",
+    desc: "Working on the frontend of the web application.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://drive.google.com/file/d/1m0PlqoxZd1CkcTcdNHyu-E7-qOW28N6l/view?usp=drivesdk",
+  },
+];
+
+
+export const projects = [
+  {
+    id: 11,
+    title: "WanderLust",
+    date: "Dec 2023 - Jan 2024",
+    description:
+      "We implemented a full stack porject using MVC framework. Basically, it is an online marketplace that connects people who want to rent out their property with people who are looking for accommodations.",
+    image:
+      "https://github.com/Tarungupta18/upload-img/blob/aim/portfolio-img/WanderLust.png?raw=true",
+    tags: [
+      "EJS",
+      "CSS",
+      "JavsScript",
+      "Node Js",
+      "Express Js",
+      "MongoDB",
+      "Passport Js",
+    ],
+    category: "web app",
+    github: "https://github.com/Tarungupta18/Delta-Project",
+    webapp: "https://delta-project-j05m.onrender.com/listings",
+  },
+  {
+    id: 9,
+    title: "Weather-App",
+    date: "Jan 2024 - Jan 2024",
+    description:
+      "We implemented a Material UI (React Framework) project weather display site. we uses a weather api for real time date sharing and displaying in a card with relatable image.",
+    image:
+      "https://github.com/Tarungupta18/upload-img/blob/aim/portfolio-img/weather-app.png?raw=true",
+    tags: [
+      "Html",
+      "CSS",
+      "JavaScript",
+      "React Js",
+      "Material UI",
+      "Weather-API",
+    ],
+    category: "web app",
+    github: "https://github.com/Tarungupta18/Weather-App.git",
+    webapp: "https://weather-app-ko7s.onrender.com",
+  },
+];
+
